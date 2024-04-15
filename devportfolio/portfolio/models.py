ab311c5ab7ebde9116c0c828c452f065457d6f62
Danny Nguyen <dqngyen1@memphis.edu>
2024-04-15 11:30:51 -0500

    completed act3

--- devportfolio/portfolio/models.py
+++ devportfolio/portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    technologies = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
